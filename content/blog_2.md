a2ef6bcb88ff81317ef1bbeb22bc2adf16d3f6dc
Michael Lehotay <mlehotay@gmail.com>
2019-12-08 19:42:25 -0500

    site setup and example posts

---
+++ content/blog_2.md
@@ -0,0 +1,9 @@
+Title: My Second Blog
+Date: 2019-12-07 09:19
+Slug: blog2
+
+```python
+print('pancakes')
+```
+
+    pancakes
---
+++ pelicanconf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Michael Lehotay'
+SITENAME = 'Mild Deutans Unite!'
+SITEURL = 'https://mlehotay.github.io/blog'
+PATH = 'content'
+STATIC_PATHS = ['images']
+TIMEZONE = 'America/Toronto'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
